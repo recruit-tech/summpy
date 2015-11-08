6813fd57a85da892063a34ccf64e6d8d9ddc7e41
Shumpei IINUMA <shumpei.iinuma@gmail.com>
2015-11-09 07:16:43 +0900

    update README and add setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import os
+import codecs
+
+
+_this_dir = os.path.abspath(os.path.dirname(__file__))
+long_description = codecs.open(
+    os.path.join(_this_dir, 'README.md'), encoding='utf-8'
+).read()
+
+setup(
+    name='summpy',
+    version='0.2',
+    description='Text summarization (sentence extraction) module. (currently, support Japanese only)',
+    long_description=long_description,
+    packages=['summpy'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7'
+    ],
+    license='MIT',
+)
